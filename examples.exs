eb508342b125bea92d00b3f1e58a03d3b4f69a6f
dingo sky <paul@dingosky.com>
2018-04-27 15:31:35 -0700

    Update examples

--- examples.exs
+++ examples.exs
@@ -18,18 +18,9 @@ alias EntropyString.CharSet
 defmodule(Id, do: use(EntropyString))
 
 IO.puts("Id: Predefined base 32 CharSet")
-IO.puts("  Characters: #{Id.charset()}")
-IO.puts("  Session ID: #{Id.session()}\n")
-
-# --------------------------------------------------------------------------------------------------
-# Base64Id
-#   Predefined URL and file system safe characters
-# --------------------------------------------------------------------------------------------------
-defmodule(Base64Id, do: use(EntropyString, charset: CharSet.charset64()))
-
-IO.puts("Base64Id: Predefined URL and file system safe CharSet")
-IO.puts("  Characters: #{Base64Id.charset()}")
-IO.puts("  Session ID: #{Base64Id.session()}\n")
+IO.puts("  Bits:       #{Id.bits()}")
+IO.puts("  Characters: #{Id.chars()}")
+IO.puts("  Random ID:  #{Id.string()}\n")
 
 # --------------------------------------------------------------------------------------------------
 # Hex Id
@@ -37,44 +28,50 @@ IO.puts("  Session ID: #{Base64Id.session()}\n")
 # --------------------------------------------------------------------------------------------------
 defmodule(Hex, do: use(EntropyString, charset: :charset16))
 
-IO.puts("Hex: Predefined hex CharSet")
-IO.puts("  Characters: #{Hex.charset()}")
-IO.puts("  Small ID: #{Hex.small()}\n")
+IO.puts("Hex: Predefined hex character session id")
+IO.puts("  Bits:       #{Id.bits()}")
+IO.puts("  Characters: #{Hex.chars()}")
+IO.puts("  Random ID:  #{Hex.string()}\n")
+
+# --------------------------------------------------------------------------------------------------
+# Base64 Id
+#   Predefined URL and file system safe characters
+# --------------------------------------------------------------------------------------------------
+defmodule(Base64Id, do: use(EntropyString, charset: charset64))
+
+IO.puts("Base64Id: Predefined URL and file system safe characters")
+IO.puts("  Bits:       #{Id.bits()}")
+IO.puts("  Characters: #{Base64Id.chars()}")
+IO.puts("  Session ID: #{Base64Id.session()}\n")
 
 # --------------------------------------------------------------------------------------------------
 # Uppercase Hex Id
 #   Uppercase hex characters
 # --------------------------------------------------------------------------------------------------
-defmodule(UpperHex, do: use(EntropyString, charset: "0123456789ABCDEF"))
+defmodule(UpperHex, do: use(EntropyString, bits: 64, charset: "0123456789ABCDEF"))
 
 IO.puts("UpperHex: Upper case hex CharSet")
-IO.puts("  Characters: #{UpperHex.charset()}")
-IO.puts("  Medium ID: #{UpperHex.medium()}\n")
+IO.puts("  Bits:       #{UpperHex.bits()}")
+IO.puts("  Characters: #{UpperHex.chars()}")
+IO.puts("  Random ID:  #{UpperHex.string()}\n")
 
 # --------------------------------------------------------------------------------------------------
 # DingoSky
-#   A million potential strings with a 1 in a billion chance of repeat
+#   Ten million strings with a 1 in a billion chance of repeat
 # --------------------------------------------------------------------------------------------------
-defmodule DingoSky do
-  use EntropyString, charset: "dingosky"
+defmodule(DingoSky, do: use(EntropyString, charset: "dingosky", total: 1.0e7, risk: 1.0e9))
 
-  @bits bits(1.0e6, 1.0e9)
-
-  def id, do: DingoSky.random(@bits)
-end
-
-IO.puts("DingoSky: Custom CharSet for a million IDs with a 1 in a billion chance of repeat")
-IO.puts("  Characters: #{DingoSky.charset()}")
-IO.puts("  DingoSky ID: #{DingoSky.id()}\n")
+IO.puts("DingoSky: Custom characters for a million IDs with a 1 in a billion chance of repeat")
+IO.puts("  Bits:        #{DingoSky.bits()}")
+IO.puts("  Characters:  #{DingoSky.chars()}")
+IO.puts("  DingoSky ID: #{DingoSky.string()}\n")
 
 # --------------------------------------------------------------------------------------------------
-# WebServer
+# Server
 #   256 entropy bit token
 # --------------------------------------------------------------------------------------------------
-defmodule WebServer do
-  use EntropyString, charset: CharSet.charset64()
-end
+defmodule(Server, do: use(EntropyString, charset: charset64))
 
-IO.puts("WebServer: 256 entropy bit token")
-IO.puts("  Characters: #{WebServer.charset()}")
-IO.puts("  MyServer Token: #{WebServer.token()}\n")
+IO.puts("Server: 256 entropy bit token")
+IO.puts("  Characters: #{Server.chars()}")
+IO.puts("  Token:      #{Server.token()}\n")
